3dd25dc79010ec727291e918821ef427a3790a27
Marcel Kedziora <nuage.clan@gmail.com>
2025-01-08 16:58:40 +0000

    finalised exercise, no styling input

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.destroy_all
+
+restaurants = [
+  { name: "Epicure", address: "75008 Paris", phone_number: "123456789", category: "french" },
+  { name: "Sushi Saito", address: "Tokyo, Japan", phone_number: "987654321", category: "japanese" },
+  { name: "La Pizzeria", address: "Rome, Italy", phone_number: "112233445", category: "italian" },
+  { name: "Le Gourmet", address: "Brussels, Belgium", phone_number: "667788990", category: "belgian" },
+  { name: "Chinatown", address: "London, UK", phone_number: "556677889", category: "chinese" }
+]
+
+restaurants.each do |restaurant|
+  Restaurant.create!(restaurant)
+end
